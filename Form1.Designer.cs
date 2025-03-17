2eafb17004db6138a2e86858a8d44e5f08a83a66
DIEGO DA SILVA LIRA <diego.slira2@senacsp.edu.br>
2025-03-17 15:22:40 -0300

    primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace alertas
+{
+    partial class frmAlertas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.btnAlertaSimples = new System.Windows.Forms.Button();
+            this.btnAlertaRobusto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(255, 9);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(294, 31);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Teste os alertas abaixo";
+            // 
+            // btnAlertaSimples
+            // 
+            this.btnAlertaSimples.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(103)))), ((int)(((byte)(86)))));
+            this.btnAlertaSimples.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlertaSimples.ForeColor = System.Drawing.Color.White;
+            this.btnAlertaSimples.Location = new System.Drawing.Point(313, 71);
+            this.btnAlertaSimples.Name = "btnAlertaSimples";
+            this.btnAlertaSimples.Size = new System.Drawing.Size(183, 31);
+            this.btnAlertaSimples.TabIndex = 1;
+            this.btnAlertaSimples.Text = " Alerta Simples";
+            this.btnAlertaSimples.UseVisualStyleBackColor = false;
+            this.btnAlertaSimples.Click += new System.EventHandler(this.btnAlertaSimples_Click);
+            // 
+            // btnAlertaRobusto
+            // 
+            this.btnAlertaRobusto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAlertaRobusto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlertaRobusto.FlatAppearance.BorderSize = 0;
+            this.btnAlertaRobusto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnAlertaRobusto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnAlertaRobusto.Location = new System.Drawing.Point(338, 126);
+            this.btnAlertaRobusto.Name = "btnAlertaRobusto";
+            this.btnAlertaRobusto.Size = new System.Drawing.Size(144, 36);
+            this.btnAlertaRobusto.TabIndex = 2;
+            this.btnAlertaRobusto.Text = "Alerta Robusto";
+            this.btnAlertaRobusto.UseVisualStyleBackColor = false;
+            this.btnAlertaRobusto.Click += new System.EventHandler(this.btnAlertaRobusto_Click);
+            // 
+            // frmAlertas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAlertaRobusto);
+            this.Controls.Add(this.btnAlertaSimples);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "frmAlertas";
+            this.Text = "Alertas";
+            this.Click += new System.EventHandler(this.frmAlertas_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Button btnAlertaSimples;
+        private System.Windows.Forms.Button btnAlertaRobusto;
+    }
+}
+
